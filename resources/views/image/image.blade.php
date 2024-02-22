788d2d7aaee330f8e6d59b41eea15e8997b6f067
eavlongsok <esok@paragoniu.edu.kh>
2024-02-22 10:48:25 +0700

    removed result from image page

--- resources/views/image/image.blade.php
+++ resources/views/image/image.blade.php
@@ -29,20 +29,26 @@
 
         .buttons {
             display: flex;
-            flex-direction: column; /* Display buttons in a column */
+            flex-direction: column;
+            /* Display buttons in a column */
         }
 
         .button {
-            margin-bottom: 10px; /* Adjust the margin between buttons as needed */
+            margin-bottom: 10px;
+            /* Adjust the margin between buttons as needed */
             padding: 10px;
             background-color: #333;
             color: #fff;
             border: none;
             cursor: pointer;
-            width: 95%; /* Make buttons take up full width */
-            text-align: center; /* Center text within buttons */
-            text-decoration: none; /* Remove underlines from anchor tags */
+            width: 95%;
+            /* Make buttons take up full width */
+            text-align: center;
+            /* Center text within buttons */
+            text-decoration: none;
+            /* Remove underlines from anchor tags */
         }
+
     </style>
 </head>
 <body>
@@ -51,7 +57,7 @@
         <div class="buttons">
             <a href="{{ route('image.encrypt') }}" class="button"> Encryption</a>
             <a href="{{ route('image.decrypt') }}" class="button"> Decryption</a>
-            <a href="{{ route('image.result') }}" class="button"> Result</a>
+            {{-- <a href="{{ route('image.result') }}" class="button"> Result</a> --}}
             <a href="{{ url('/') }}" class="button"> Back to Menu</a>
         </div>
     </div>
